d0bf1c93ff4ef1948732cfa3fc21be1eda888c03
ianmuler <yoelmuler@gmail.com>
2025-12-05 16:05:37 -0300

    fix: service worker

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,24 @@
-const CACHE_NAME = 'tti-outfits-v7';
-
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/data/combos.json',
-  '/css/base.css',
-  '/css/bienvenida.css',
-  '/css/buscador.css',
-  '/css/talles.css',
-  '/js/app.js',
-  '/js/buscador-outfits.js',
-  '/js/calcular-talles.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
+// Service Worker mínimo para PWA instalable (sin cache offline)
+self.addEventListener('install', function() {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
+  // Borrar todos los caches viejos
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then(function(names) {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+        names.map(function(name) {
+          return caches.delete(name);
         })
       );
-    }).then(() => self.clients.claim())
+    }).then(function() {
+      return self.clients.claim();
+    })
   );
 });
 
-// Estrategia: Network-first para todo (cache solo para offline)
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        // Guardar copia en cache
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => {
-        // Sin conexión: usar cache
-        return caches.match(event.request);
-      })
-  );
+// No interceptar requests - dejar que el navegador maneje todo normalmente
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
